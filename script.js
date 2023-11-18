c59484225db6e5ef8414a1b39a4a929fd9fdd756
GasanaJr <gasanadj2003@gmail.com>
2023-11-18 05:17:20 +0200

    ft(about): add about and it's transitions

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const animatedText = document.querySelector(".animated-text");
   const sections = document.querySelectorAll(".hidden-content");
+  const homeImg = document.getElementById("home-img");
   const body = document.querySelector(".body");
   setTimeout(function () {
     animatedText.classList.add("hide");
@@ -8,5 +9,34 @@ document.addEventListener("DOMContentLoaded", function () {
     sections.forEach((section, index) => {
       section.style.opacity = 1;
     });
-  }, 9000);
+    homeImg.style.animation = "throwUp 4s ease 0s forwards";
+  }, 5000);
 });
+
+function handleIntersection(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const span = document.querySelectorAll(".h1 span");
+      const numbers = document.querySelectorAll(".number");
+      const dataNum = document.querySelectorAll(".data");
+      const image = document.querySelector(".img-strawberry");
+      span.forEach((el) => {
+        el.style.opacity = 1;
+      });
+      numbers.forEach((el) => {
+        el.style.opacity = 1;
+      });
+      dataNum.forEach((el) => {
+        el.style.opacity = 1;
+      });
+      image.style.animation = "throwIn 5s ease 0s forwards";
+
+      observer.unobserve(entry.target);
+    }
+  });
+}
+const produceSection = document.querySelector(".produce");
+const observer = new IntersectionObserver(handleIntersection, {
+  threshold: 0.5,
+});
+observer.observe(produceSection);
