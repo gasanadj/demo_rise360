7e9a24f41e5be3958cdd1c97bed869f54708e247
GasanaJr <gasanadj2003@gmail.com>
2023-11-25 03:02:46 +0200

    ft(contact): add contact section

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const body = document.querySelector(".body");
   setTimeout(function () {
     animatedText.classList.add("hide");
-    body.style.backgroundColor = "#007aff";
+    body.style.backgroundColor = "hsla(158, 23%, 18%, 1)";
     sections.forEach((section, index) => {
       section.style.opacity = 1;
     });
@@ -100,4 +100,157 @@ document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("scroll", adjustHeightOnScroll);
   window.addEventListener("resize", adjustHeightOnScroll);
   adjustHeightOnScroll();
+});
+
+// contact section
+
+let buttons = document.querySelectorAll(".bq > button");
+
+buttons.forEach(addButtonListener);
+
+function addButtonListener(button) {
+  button.addEventListener("click", function () {
+    const userMessage = this.innerText;
+
+    // Append user message to chat
+    appendMessage(userMessage, "user");
+
+    // Generate and append bot's reply
+    let replies = getBotReply(userMessage);
+    appendBotReplies(replies);
+  });
+}
+
+function appendMessage(message, sender) {
+  const messagesDiv = document.querySelector(".messages");
+  const messageDiv = document.createElement("div");
+  messageDiv.className = `${sender}-message`;
+  messageDiv.innerText = message;
+  messagesDiv.appendChild(messageDiv);
+  messagesDiv.scrollTop = messagesDiv.scrollHeight;
+
+  scrollToBottom();
+}
+
+function getBotReply(userMessage) {
+  switch (userMessage) {
+    case "Tell me about RiseFarmer360":
+      return [
+        "RiseFarmer360 is a transformative web application designed to support farmers in Sub-Saharan Africa.",
+        "It addresses challenges by offering modern farming resources, a knowledge-sharing network, a marketplace, and localized weather forecasts.",
+        "What specific aspect are you interested in?",
+      ];
+    case "How does RiseFarmer360 help farmers?":
+      return [
+        "RiseFarmer360 empowers farmers through a comprehensive approach.",
+        "It provides resources on modern farming techniques, fosters collaboration through a knowledge-sharing network, facilitates market access, and offers localized weather forecasts.",
+        "The goal is to enhance productivity, resilience, and prosperity for Sub-Saharan farmers.",
+      ];
+    case "What makes RiseFarmer360 unique?":
+      return [
+        "RiseFarmer360 stands out by combining education, market facilitation, and climate technology in one platform.",
+        "The innovative peer-to-peer learning network promotes community-driven progress and empowerment.",
+        "This approach fosters a sustainable growth mindset, allowing farmers to embrace agricultural techniques and seize market opportunities.",
+      ];
+    case "How can farmers access RiseFarmer360?":
+      return [
+        "Farmers can access RiseFarmer360 through the web application.",
+        "They can utilize the platform for modern farming resources, collaboration, market access, and weather forecasts.",
+        "The goal is to transform farming practices and improve the lives of millions of hardworking farmers.",
+      ];
+    case "What are the ultimate goals of RiseFarmer360?":
+      return [
+        "RiseFarmer360 aims to drive development, enhance food security, and improve the lives of millions of hardworking farmers in Sub-Saharan Africa.",
+        "By consolidating services into one platform, the project strives to transform farming practices and contribute to the overall prosperity of the farming community.",
+      ];
+    case "Who are the brains behind RiseFarmer360?":
+      return [
+        "RiseFarmer360 was developed by a team of six students",
+        "Kathrine Ganda",
+        "Grace Nalule",
+        "Irene Agigiba",
+        "Charite Uwatwembi",
+        "Mary Doh",
+        "Didas Junior",
+      ];
+    default:
+      return ["Sorry, I don't understand that."];
+  }
+}
+
+function appendQuestionButtons() {
+  const messagesDiv = document.querySelector(".messages");
+
+  const questions = [
+    "Tell me about RiseFarmer360",
+    "How does RiseFarmer360 help farmers?",
+    "What makes RiseFarmer360 unique?",
+    "How can farmers access RiseFarmer360?",
+    "What are the ultimate goals of RiseFarmer360?",
+    "Who are the brains behind RiseFarmer360?",
+  ];
+
+  const buttonGroup = document.createElement("div");
+  buttonGroup.className = "button-holder-group";
+
+  questions.forEach((question) => {
+    const buttonHolder = document.createElement("div");
+    buttonHolder.className = "button-holder bq";
+
+    const button = document.createElement("button");
+    button.className = "bq";
+    button.innerText = question;
+
+    buttonHolder.appendChild(button);
+    buttonGroup.appendChild(buttonHolder);
+
+    // Add the click event listener to the newly created button
+    addButtonListener(button);
+  });
+
+  messagesDiv.appendChild(buttonGroup);
+
+  scrollToBottom();
+}
+
+// Append the initial set of question buttons when the chatbot loads
+appendQuestionButtons();
+
+function scrollToBottom() {
+  const messagesDiv = document.querySelector(".messages");
+  const lastChild = messagesDiv.lastChild;
+  lastChild.scrollIntoView({ behavior: "smooth" });
+}
+
+function appendBotReplies(replies, index = 0) {
+  if (index < replies.length) {
+    setTimeout(() => {
+      appendMessage(replies[index], "bot");
+      appendBotReplies(replies, index + 1);
+    }, 1000); // 1 second delay
+  } else {
+    // After all bot replies, display the question buttons again.
+    appendQuestionButtons();
+  }
+}
+
+// close the modal
+document.addEventListener("DOMContentLoaded", function () {
+  const closeButton = document.querySelector(".close");
+  const contactSection = document.querySelector(".contact");
+
+  closeButton.addEventListener("click", function () {
+    contactSection.style.display = "none";
+  });
+});
+
+const btn = document.getElementById("cont");
+const contactSection = document.querySelector(".contact");
+btn.addEventListener("click", () => {
+  contactSection.style.display = "block";
+});
+
+const btn2 = document.getElementById("cont1");
+btn2.addEventListener("click", () => {
+  contactSection.style.display = "block";
 });
