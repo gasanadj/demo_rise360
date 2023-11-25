fab1d06d57797703c248f7550f9adca460482672
GasanaJr <gasanadj2003@gmail.com>
2023-11-25 04:39:06 +0200

    ft(home): add market intro

--- script.js
+++ script.js
@@ -47,60 +47,60 @@ drops.forEach((drop) => {
   drop.style.animationDuration = 20 / dur + "s";
 });
 
-// Expanding cards
-document.addEventListener("DOMContentLoaded", function () {
-  const ids = ["pro1", "pro2", "pro3", "pro4"];
-  const elements = ids.map((id) => document.getElementById(id));
-  let expandingIndex = 0;
-  let lastScrollTop = 0;
+// // Expanding cards
+// document.addEventListener("DOMContentLoaded", function () {
+//   const ids = ["pro1", "pro2", "pro3", "pro4"];
+//   const elements = ids.map((id) => document.getElementById(id));
+//   let expandingIndex = 0;
+//   let lastScrollTop = 0;
 
-  function adjustHeightOnScroll() {
-    const currentScrollTop = window.scrollY || window.pageYOffset;
+//   function adjustHeightOnScroll() {
+//     const currentScrollTop = window.scrollY || window.pageYOffset;
 
-    elements.forEach((current, index) => {
-      var rect = current.getBoundingClientRect();
-      const isScrollingDown = currentScrollTop > lastScrollTop;
+//     elements.forEach((current, index) => {
+//       var rect = current.getBoundingClientRect();
+//       const isScrollingDown = currentScrollTop > lastScrollTop;
 
-      if (
-        (isScrollingDown &&
-          rect.top < window.innerHeight &&
-          rect.bottom >= 0) ||
-        (!isScrollingDown && rect.bottom > 0 && rect.top <= window.innerHeight)
-      ) {
-        var height = window.innerHeight - rect.top;
-        if (index === expandingIndex) {
-          const maxHeight = 400;
-          if (height <= maxHeight) {
-            current.style.transition = "height 0.2s ease-in-out";
-            current.style.height = height + "px";
-            current.classList.add("expanding-element");
-          }
-          if (height >= maxHeight) {
-            expandingIndex = isScrollingDown
-              ? Math.min(expandingIndex + 1, elements.length - 1)
-              : Math.max(expandingIndex - 1, 0);
-          }
-        } else {
-          const originalHeight = 0;
-          current.style.transition = "height 0.2s ease-in-out";
-          current.style.height = originalHeight + "px";
-          current.classList.remove("expanding-element");
-        }
-      } else {
-        const originalHeight = 0;
-        current.style.transition = "height 0.1s ease-in-out";
-        current.style.height = originalHeight + "px";
-        current.classList.remove("expanding-element");
-      }
-    });
+//       if (
+//         (isScrollingDown &&
+//           rect.top < window.innerHeight &&
+//           rect.bottom >= 0) ||
+//         (!isScrollingDown && rect.bottom > 0 && rect.top <= window.innerHeight)
+//       ) {
+//         var height = window.innerHeight - rect.top;
+//         if (index === expandingIndex) {
+//           const maxHeight = 400;
+//           if (height <= maxHeight) {
+//             current.style.transition = "height 0.2s ease-in-out";
+//             current.style.height = height + "px";
+//             current.classList.add("expanding-element");
+//           }
+//           if (height >= maxHeight) {
+//             expandingIndex = isScrollingDown
+//               ? Math.min(expandingIndex + 1, elements.length - 1)
+//               : Math.max(expandingIndex - 1, 0);
+//           }
+//         } else {
+//           const originalHeight = 0;
+//           current.style.transition = "height 0.2s ease-in-out";
+//           current.style.height = originalHeight + "px";
+//           current.classList.remove("expanding-element");
+//         }
+//       } else {
+//         const originalHeight = 0;
+//         current.style.transition = "height 0.1s ease-in-out";
+//         current.style.height = originalHeight + "px";
+//         current.classList.remove("expanding-element");
+//       }
+//     });
 
-    lastScrollTop = currentScrollTop;
-  }
+//     lastScrollTop = currentScrollTop;
+//   }
 
-  window.addEventListener("scroll", adjustHeightOnScroll);
-  window.addEventListener("resize", adjustHeightOnScroll);
-  adjustHeightOnScroll();
-});
+//   window.addEventListener("scroll", adjustHeightOnScroll);
+//   window.addEventListener("resize", adjustHeightOnScroll);
+//   adjustHeightOnScroll();
+// });
 
 // contact section
 
